c187fef7b32144bbd1fc818d5ab33fa9ab4f8dfb
jjj3108 <jjj3108@naver.com>
2022-04-08 23:38:44 +0900

    0.03v logback add

--- mydata/src/main/java/mdata/MydataApplication.java
+++ mydata/src/main/java/mdata/MydataApplication.java
@@ -1,5 +1,6 @@
 package mdata;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"mdata.com", "mdata.md"})
 public class MydataApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MydataApplication.class, args);
 		
