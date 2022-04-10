81ee65e97bb08613af7152e038a35fd709653589
jjj3108 <jjj3108@naver.com>
2022-04-10 22:19:20 +0900

    20220410 - 화면에 helloHome 출력

--- mydata/src/main/java/mdata/md/controller/MAIN0010Controller.java
+++ mydata/src/main/java/mdata/md/controller/MAIN0010Controller.java
@@ -1,10 +1,27 @@
 package mdata.md.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import mdata.md.service.MAIN0010Service;
 
 @Controller
 public class MAIN0010Controller {
-	public boolean isTrue() {
-		return true;
+	
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	MAIN0010Service main0010Service;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String MAIN0010() {
+		logger.info("MAIN0010 START");
+		return "helloHome";
 	}
 }
