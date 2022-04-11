f385155c724aaad088681895cb1acf27e6dbd9f1
jjj31 <jjj31@DESKTOP-389PLET>
2022-04-11 10:43:27 +0900

    202204111043 COMMIT, PUSH TEST

--- mydata/src/main/java/mdata/md/controller/MAIN0010Controller.java
+++ mydata/src/main/java/mdata/md/controller/MAIN0010Controller.java
@@ -1,12 +1,16 @@
 package mdata.md.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import mdata.md.service.MAIN0010Service;
 
@@ -18,10 +22,17 @@ public class MAIN0010Controller {
 	@Autowired
 	MAIN0010Service main0010Service;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/MAIN0010", method = RequestMethod.GET)
 	@ResponseBody
-	public String MAIN0010() {
+	public ModelAndView viewMAIN0010(@RequestParam(required = false) Map<String, Object> params) {
+		
 		logger.info("MAIN0010 START");
-		return "helloHome";
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/MAIN0010");
+		
+		mv.addObject("INBOUND01", params);
+
+		return mv;
 	}
 }
---
+++ mydata/src/main/java/mdata/md/dao/MAIN0010Dao.java
@@ -0,0 +1,6 @@
+package mdata.md.dao;
+
+//@Mapper
+public interface MAIN0010Dao {
+	public boolean isTrue();
+}
