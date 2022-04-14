d1adcaa0807466bab37ca87d464f062b612978ee
jjj3108 <jjj3108@naver.com>
2022-04-14 14:49:57 +0900

    202204141449 interceptor추가

---
+++ mydata/src/main/java/mdata/common/interceptor/myinterceptor.java
@@ -0,0 +1,45 @@
+package mdata.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class myinterceptor implements HandlerInterceptor{
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("myinterceptor preHandle");
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("myinterceptor posthandle");
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+
+
+
+	
+
+
+}
